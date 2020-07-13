2a081ef6d9f1394f884a7851eaaceaf113608935
kevinadhiguna <kevin26798@gmail.com>
2020-07-13 16:49:24 +0700

    refactor: version0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Meme } from './components/Meme';
+
+const objectToQueryParam = obj => {
+  const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`);
+  return '?' + params.join('&');
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [templates, setTemplates] = useState([]);
+  const [template, setTemplate] = useState(null);
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [meme, setMeme] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then(x => x.json()
+        .then(response => setTemplates(response.data.memes)))
+  }, []);
+
+  if(meme) {
+    return (
+      <div style={{textAlign: "center"}}>
+        <img 
+          style={{width: 200}}
+          src={meme}
+          alt="customized meme"
+        />
+      </div>
+    );
+  }
+
+  return ( 
+    <>
+      <div 
+        className="App"
+        //style={{textAlign: "center"}} 
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1> Meme Generator </h1>
+        {template && (
+          <form 
+            onSubmit={async e => {
+              e.preventDefault();
+              // add logic to generate meme from API
+              const params = {
+                template_id: template.id,
+                text0: topText,
+                text1: bottomText,
+                username: "xzk03017",
+                password: "xzk03017@cndps.com"
+                /* Should have used below (better practice) */
+                //username: process.env.REACT_APP_IMGFLIP_USERNAME,
+                //password: process.env.REACT_APP_IMGFLIP_PASSWORD
+              } 
+              const response = await fetch(
+                `https://api.imgflip.com/caption_image${objectToQueryParam(
+                  params
+                )}`
+              );
+              const json = await response.json();
+              setMeme(json.data.url);
+            }}
+          >
+            <Meme template={template} />
+            <input 
+              placeholder="top text" 
+              value={topText}
+              onChange={e => setTopText(e.target.value)}
+            />
+            <input 
+              placeholder="bottom text" 
+              value={bottomText}
+              onChange={e => setBottomText(e.target.value)}
+            />
+            <button type="submit">Generate Meme!</button>
+          </form>
+        )}
+        {!template && (
+          <>
+            <h1>Pick a template !</h1>
+            {templates.map(template => {
+              return (
+                <Meme
+                  template={template} 
+                  onClick={() => {
+                    setTemplate(template);
+                  }}
+                />
+              );
+            })}
+          </>
+        )} 
+      </div> 
+    </>
   );
 }
 
